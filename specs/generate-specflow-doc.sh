eab7c46363c886ac8f14211d8ab6a71e622c04ba
Marc LION <lionmarc@free.fr>
2022-10-15 08:38:37 +0200

    TECH: Generate specflow living doc (#1)
    * TECH: Generate specflow living doc
    
    * Set script as executable
    
    * Restore tools in workflow
    
    * Try to fix path
    
    * update workflows

---
+++ specs/generate-specflow-doc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+DIR="$(realpath "${DIR}")"
+
+RESULTS_FOLDER=$DIR/../.specflow_results
+mkdir -p $RESULTS_FOLDER
+
+for f in $DIR/*; do
+    if [ -d "$f" ]; then
+        assembly="$(basename $f)"
+        sedScript='s@"FeatureFolderPath":"@"FeatureFolderPath":"'$assembly'/@g'
+        sed -e $sedScript $DIR/$assembly/bin/TestExecution.json > $RESULTS_FOLDER/TestExecution_$assembly.json
+    fi
+done
+
+dotnet livingdoc feature-folder $DIR --title "SMuSDI core" -o $RESULTS_FOLDER -t $RESULTS_FOLDER/TestExecution*.json
