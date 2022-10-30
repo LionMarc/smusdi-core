d8551e55c081698536c645dffd1a04fa366f61c7
Marc LION <lionmarc@free.fr>
2022-10-30 13:34:39 +0100

    Add helper to initialize database table in tests

---
+++ testing/Smusdi.Testing/Database/SmusdiTestingServiceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace Smusdi.Testing.Database;
+
+public static class SmusdiTestingServiceExtensions
+{
+    public static async Task StoreItemsInDatabase<TContext, TDao>(this SmusdiTestingService smusdiTestingService, Table table, Func<Table, IEnumerable<TDao>>? itemsCreator = null)
+        where TContext : DbContext
+        where TDao : class
+    {
+        var items = itemsCreator?.Invoke(table) ?? table.CreateSet<TDao>();
+        if (items == null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var provider = smusdiTestingService.GetService<IServiceProvider>();
+        if (provider == null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        using var scope = provider.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<TContext>();
+        await dbContext.Set<TDao>().AddRangeAsync(items);
+        await dbContext.SaveChangesAsync();
+    }
+}
