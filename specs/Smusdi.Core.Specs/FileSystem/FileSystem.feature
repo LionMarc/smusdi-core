f820e6e6615d3ba0b1e041a168f743584ba1d631
Marc LION <lionmarc@free.fr>
2022-11-02 18:34:08 +0100

    Add steps to test files

---
+++ specs/Smusdi.Core.Specs/FileSystem/FileSystem.feature
@@ -0,0 +1,26 @@
+﻿Feature: FileSystem
+
+This feature is used to tests the specflow steps associated to the file system mock.
+I decide to use [System.IO.Abstractions](https://github.com/TestableIO/System.IO.Abstractions) in services and System.IO.Abastractions.TestingHelpers in tests.
+
+Background: 
+    Given the service initialized and started
+
+Scenario: Adding a file and testing that it exists
+    When I create the file "K:/subfolder/test.txt" with content
+    """
+    Content of the file
+    """
+    Then the file "K:/subfolder/test.txt" exists
+    And the file "K:/subfolder/test.txt" has content
+    """
+    Content of the file
+    """
+
+Scenario: Removing a file and testing that it exists
+    Given the file "K:/subfolder/test.txt" with content
+    """
+    Content of the file
+    """
+    When I delete the file "K:/subfolder/test.txt"
+    Then the file "K:/subfolder/test.txt" does not exist
--- testing/Smusdi.Testing/FileSystem/FileSystemSteps.cs
+++ testing/Smusdi.Testing/FileSystem/FileSystemSteps.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO.Abstractions;
+using FluentAssertions;
 using TechTalk.SpecFlow;
 
 namespace Smusdi.Testing.FileSystem;
@@ -6,14 +7,43 @@ namespace Smusdi.Testing.FileSystem;
 [Binding]
 public sealed class FileSystemSteps
 {
-    private readonly IFileSystem? fileSystem;
+    private readonly IFileSystem fileSystem;
 
-    public FileSystemSteps(SmusdiTestingService smusdiTestingService) => this.fileSystem = smusdiTestingService.GetService<IFileSystem>();
+    public FileSystemSteps(SmusdiTestingService smusdiTestingService) => this.fileSystem = smusdiTestingService.GetService<IFileSystem>() ?? throw new NullReferenceException("IFileSystem");
 
     [Given(@"the file ""(.*)"" with content")]
+    [When(@"I create the file ""(.*)"" with content")]
     public void GivenTheFileWithContent(string p0, string multilineText)
     {
-        this.fileSystem?.Directory.CreateDirectory(Path.GetDirectoryName(p0));
-        this.fileSystem?.File.WriteAllText(p0, multilineText);
+        this.fileSystem.Directory.CreateDirectory(Path.GetDirectoryName(p0));
+        this.fileSystem.File.WriteAllText(p0, multilineText);
+    }
+
+    [When(@"I delete the file ""(.*)""")]
+    public void WhenIDeleteTheFile(string p0)
+    {
+        this.fileSystem.File.Delete(p0);
+    }
+
+    [Then(@"the file ""(.*)"" does not exist")]
+    public void ThenTheFileDoesNotExist(string p0)
+    {
+        this.fileSystem.File.Exists(p0).Should().BeFalse();
+    }
+
+    [Then(@"the file ""(.*)"" exists")]
+    public void ThenTheFileExists(string p0)
+    {
+        this.fileSystem.File.Exists(p0).Should().BeTrue();
+    }
+
+    [Then(@"the file ""(.*)"" has content")]
+    public void ThenTheFileHasContent(string p0, string multilineText)
+    {
+        this.fileSystem.File.Exists(p0).Should().BeTrue();
+        using var stream = this.fileSystem.File.OpenRead(p0);
+        using var reader = new StreamReader(stream);
+        var content = reader.ReadToEnd();
+        content.Should().Be(multilineText);
     }
 }
