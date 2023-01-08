cfbe169d42e870d4f556fa9a4f94943f83c1d844
Marc LION <lionmarc@free.fr>
2023-01-08 15:54:54 +0100

    Add tests helpers for FluentAssertions and database

---
+++ testing/Smusdi.Testing/FluentAssertionsHelpers/EquivalencyAssertionOptionsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using FluentAssertions.Equivalency;
+using TechTalk.SpecFlow;
+
+namespace Smusdi.Testing.FluentAssertionsHelpers;
+
+public static class EquivalencyAssertionOptionsExtensions
+{
+    public static EquivalencyAssertionOptions<T> WithStringNullOrEmptyComparison<T>(this EquivalencyAssertionOptions<T> options)
+        => options.Using<string>(ctx => (ctx.Subject ?? string.Empty).Should().BeEquivalentTo(ctx.Expectation ?? string.Empty)).WhenTypeIs<string>();
+
+    public static EquivalencyAssertionOptions<T> CheckOnlyTableHeaders<T>(this EquivalencyAssertionOptions<T> options, Table table)
+       => options.Excluding(a => !table.Header.Contains(a.Name));
+}
