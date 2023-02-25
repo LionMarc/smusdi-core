5ab80c02faad0d7b3f1c67e74be56293712388c2
Marc LION <lionmarc@free.fr>
2023-02-25 08:23:14 +0100

    Initial version of SmusdiWorker used to execute registered tasks and stop

---
+++ src/Smusdi.Core/Worker/SmusdiWorker.cs
@@ -0,0 +1,69 @@
+﻿using System.IO.Abstractions;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Smusdi.Core.Configuration;
+using Smusdi.Core.Extensibility;
+using Smusdi.Core.Helpers;
+using Smusdi.Core.Json;
+using Smusdi.Core.Logging;
+
+namespace Smusdi.Core.Worker;
+
+public class SmusdiWorker
+{
+    public HostApplicationBuilder? HostBuilder { get; private set; }
+
+    public IHost? WorkerHost { get; private set; }
+
+    public static Task InitAndRunAsync(string[] args) => InitAndRunAsync<SmusdiWorker>(args);
+
+    public static async Task InitAndRunAsync<T>(string[] args)
+        where T : SmusdiWorker, new()
+    {
+        var worker = new T();
+        worker.CreateAndInitializeBuider(args);
+        worker.Build();
+        worker.ConfigureHost();
+
+        if (worker.WorkerHost != null)
+        {
+            await worker.WorkerHost.RunAsync();
+        }
+    }
+
+    public virtual void CreateAndInitializeBuider(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args)
+            .InitConfiguration()
+            .InitLoggerConfiguration();
+
+        builder.Services
+            .AddSingleton<IFileSystem, FileSystem>()
+            .AddSingleton<IClock, Clock>()
+            .Configure<JsonOptions>(j => j.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()))
+            .AddJsonSerializerWithJsonOptions()
+            .AddApplicationServices<IServicesRegistrator>(builder.Configuration)
+            .AddWorkerTasks(builder.Configuration)
+            .AddHostedService<WorkerTasksRunner>();
+
+        this.HostBuilder = builder;
+    }
+
+    public virtual void Build()
+    {
+        if (this.HostBuilder == null)
+        {
+            return;
+        }
+
+        this.WorkerHost = this.HostBuilder.Build();
+    }
+
+    public virtual void ConfigureHost()
+    {
+        if (this.WorkerHost == null)
+        {
+            return;
+        }
+    }
+}
---
+++ src/Smusdi.Core/Worker/WorkerTasksRunner.cs
@@ -0,0 +1,44 @@
+﻿using ILogger = Serilog.ILogger;
+
+namespace Smusdi.Core.Worker;
+
+public sealed class WorkerTasksRunner : BackgroundService
+{
+    private readonly ILogger logger;
+    private readonly IServiceProvider serviceProvider;
+    private readonly IHostApplicationLifetime hostApplicationLifetime;
+
+    public WorkerTasksRunner(ILogger logger, IServiceProvider serviceProvider, IHostApplicationLifetime hostApplicationLifetime)
+    {
+        this.logger = logger;
+        this.serviceProvider = serviceProvider;
+        this.hostApplicationLifetime = hostApplicationLifetime;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        this.logger.Information("Executing registered tasks...");
+
+        using (var scope = this.serviceProvider.CreateScope())
+        {
+            var workerTasks = scope.ServiceProvider.GetServices<IWorkerTask>();
+            foreach (var workerTask in workerTasks)
+            {
+                if (stoppingToken.IsCancellationRequested)
+                {
+                    this.logger.Information($"Worker has been cancelled.");
+                    return;
+                }
+
+                this.logger.Information($"Starting {workerTask.Name}.");
+
+                await workerTask.Execute(stoppingToken);
+
+                this.logger.Information($"{workerTask.Name} done.");
+            }
+        }
+
+        this.logger.Information("All tasks done.");
+        this.hostApplicationLifetime.StopApplication();
+    }
+}
