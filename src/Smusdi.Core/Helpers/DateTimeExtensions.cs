32d201acd6b1a98996504359b3a1d15944a17a7d
Marc LION <lionmarc@free.fr>
2023-02-27 18:48:29 +0100

    Add divers helpers (#41)

---
+++ src/Smusdi.Core/Helpers/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace Smusdi.Core.Helpers;
+
+public static class DateTimeExtensions
+{
+    public static DateTime ToUtc(this DateTime dateTime)
+    {
+        return dateTime.Kind switch
+        {
+            DateTimeKind.Utc => dateTime,
+            DateTimeKind.Local => dateTime.ToUniversalTime(),
+            DateTimeKind.Unspecified => DateTime.SpecifyKind(dateTime, DateTimeKind.Utc),
+            _ => dateTime,
+        };
+    }
+}
---
+++ testing/Smusdi.Testing/SpecFlowHelpers/TimeOnlyRetriever.cs
@@ -0,0 +1,11 @@
+﻿using TechTalk.SpecFlow.Assist.ValueRetrievers;
+
+namespace Smusdi.Testing.SpecFlowHelpers;
+
+public sealed class TimeOnlyRetriever : StructRetriever<TimeOnly>
+{
+    protected override TimeOnly GetNonEmptyValue(string value)
+    {
+        return TimeOnly.Parse(value);
+    }
+}
