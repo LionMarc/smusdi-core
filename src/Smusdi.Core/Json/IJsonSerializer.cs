e3603e605b7a8a80b5b1f4445057970701205c68
Marc LION <lionmarc@free.fr>
2023-03-09 13:11:29 +0100

    Update IJsonSerializer to provide the serialization options and to allow processing streams

--- src/Smusdi.Core/Json/IJsonSerializer.cs
+++ src/Smusdi.Core/Json/IJsonSerializer.cs
@@ -1,8 +1,16 @@
-﻿namespace Smusdi.Core.Json;
+﻿using System.Text.Json;
+
+namespace Smusdi.Core.Json;
 
 public interface IJsonSerializer
 {
+    JsonSerializerOptions JsonSerializerOptions { get; }
+
     string Serialize<T>(T value);
 
+    void Serialize<T>(T value, Stream stream);
+
     T? Deserialize<T>(string value);
+
+    T? Deserialize<T>(Stream stream);
 }
