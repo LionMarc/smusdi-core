94c0955e3a03973a22f258369e4efd17837f2db6
Marc LION <lionmarc@free.fr>
2023-04-11 22:10:22 +0200

    Allow serving any static file

--- src/Smusdi.Core/StaticSite.cs
+++ src/Smusdi.Core/StaticSite.cs
@@ -5,4 +5,6 @@ public sealed class StaticSite
     public string Folder { get; set; } = string.Empty;
 
     public string RequestPath { get; set; } = string.Empty;
+
+    public bool ServeUnknownFileTypes { get; set; }
 }
