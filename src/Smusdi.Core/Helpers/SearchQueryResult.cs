14265382deb17c2c09f0e951407becff7db0eec7
Marc LION <lionmarc@free.fr>
2023-05-18 12:05:11 +0200

    [Smusdi.PostgreSql] Initialize a search method in IAuditService

---
+++ src/Smusdi.Core/Helpers/SearchQueryResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Smusdi.Core.Helpers;
+
+public sealed class SearchQueryResult<T>
+{
+    public SearchQueryResult(int maxResults, int totalResults, IEnumerable<T> results)
+    {
+        this.MaxResults = maxResults;
+        this.TotalResults = totalResults;
+        this.Results = results;
+    }
+
+    public int MaxResults { get; }
+
+    public int TotalResults { get; }
+
+    public IEnumerable<T> Results { get; }
+}
