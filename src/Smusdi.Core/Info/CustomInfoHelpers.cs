09527a2229a5dab17b7e3762d934b626ba01e8a7
Marc LION <lionmarc@free.fr>
2023-06-25 12:27:56 +0200

    Generate a custom info file with oauth authorirty when using oauth and custom infos folder is set

---
+++ src/Smusdi.Core/Info/CustomInfoHelpers.cs
@@ -0,0 +1,6 @@
+﻿namespace Smusdi.Core.Info;
+
+public static class CustomInfoHelpers
+{
+    public static string? GetInfoFolder() => Environment.GetEnvironmentVariable(SmusdiConstants.SmusdiCustomInfoEnvVar);
+}
