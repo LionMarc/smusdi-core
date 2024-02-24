d59cdf18f576c97c4fe580360d96ab3f79772d43
Marc LION <lionmarc@free.fr>
2024-02-24 14:32:35 +0100

    Implementation of a DeferredBackgroundService

---
+++ src/Smusdi.Core/Worker/DeferredBackgroundService.cs
@@ -0,0 +1,14 @@
+﻿namespace Smusdi.Core.Worker;
+
+public abstract class DeferredBackgroundService(IHostApplicationLifetime hostApplicationLifetime) : BackgroundService
+{
+    private readonly IHostApplicationLifetime hostApplicationLifetime = hostApplicationLifetime;
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        this.hostApplicationLifetime.ApplicationStarted.Register(() => this.OnHostStarted(stoppingToken));
+        return Task.CompletedTask;
+    }
+
+    protected abstract Task OnHostStarted(CancellationToken stoppingToken);
+}
