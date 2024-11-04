14a7be8fbb074f6b9eb1a4b03ca1b8eae7ca7674
Marc LION <lionmarc@free.fr>
2024-11-04 18:03:53 +0100

    fix #107 : .env file is not read by SmusdiWorker as it is by SmusdiService

---
+++ src/Smusdi.Core/Helpers/EnvFileHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Smusdi.Core.Helpers;
+
+public static class EnvFileHelper
+{
+    public static void ReadEnvFileIfExists()
+    {
+        var envFile = Environment.GetEnvironmentVariable(SmusdiConstants.SmusdiEnvFilePath) ?? SmusdiConstants.DefaultEnvFile;
+        if (!string.IsNullOrWhiteSpace(envFile) && File.Exists(envFile))
+        {
+            foreach (var line in File.ReadAllLines(envFile))
+            {
+                var parts = line.Split('=');
+                if (parts.Length >= 2)
+                {
+                    var value = Environment.ExpandEnvironmentVariables(string.Join('=', parts.Skip(1)));
+                    Environment.SetEnvironmentVariable(parts[0], value);
+                }
+            }
+        }
+    }
+}
